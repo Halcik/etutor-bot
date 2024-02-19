66b9b31a9f6ad3217a978eb3f75629c16a8c1fc2
Halcik <julkatablet2507@gmail.com>
2024-02-19 17:52:52 +0100

    Adding voice-bot, which say when I can say a word

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import speech_recognition as sr
+import speech_recognition as sr #rozpoznawanie głosu
 import time
 import pyautogui as pg
+import pyttsx3 as tts #tworzenie głosu bota
 #PyAudio and Interned connection required
 
+
 r = sr.Recognizer()
+engine = tts.init() #inicjalizacja silnika zmiany tekstu na mowę
+engine.setProperty('rate', 200)
+
+def say():
+    engine.say("Słucham")
+    engine.runAndWait()
 
 def get_text():
     with sr.Microphone() as source:
         try:
             r.adjust_for_ambient_noise(source)
+            say()
             print("    Nasłuchuje...")
             audio = r.listen(source)
             text = r.recognize_google(audio, language='pl-PL')
@@ -29,13 +38,13 @@ while True:
             break
         elif "sprawdz" in txt:
             pg.press('enter')
-        elif "źle" in txt:
+        elif "źle" in txt or "słab" in txt:
             pg.press('1')
         elif "średn" in txt:
             pg.press('2')
         elif "świetn" in txt:
             pg.press('3')
-        time.sleep(1.2)
+        time.sleep(1.5)
 
     except:
-        pass
+        time.sleep(0.5)
